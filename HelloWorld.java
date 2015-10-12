f47ed706244ddb34162e04ec2b3e0a3c80612a15
lairen <lairen>
2015-10-12 15:24:36 +0800

    提交自己创建的项目

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.lairen.Example;
+
+/**
+ * Created by lairen on 15/10/12.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
